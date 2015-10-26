e01826780ee970a96363ea0b7d144eca9211ef7a
Steven Grissom <sggrissom@gmail.com>
2015-10-26 00:30:27 -0500

    refactored the GA code into the header file and began the hill climbing implementation

--- anne/Project2/main.cpp
+++ anne/Project2/main.cpp
@@ -10,195 +10,87 @@
 #include "string.h"
 #include "time.h"
 #include "stdio.h"
+#include <math.h>
 #include "slib.h"
-#include "ga.h"
 
-#define LINE_BREAK printf("--------\n");
+#define GA 1
+#define P1 0
+
+#define MutationRate 0.3
+#define MutationMagnitude 10
+#define GenerationCount 50
+#define PopulationSize 1000
+#define ChromosomeSize 2
+
+#if P1
+#define LowerBound -2
+#define UpperBound 4
+#else
+#define LowerBound -10
+#define UpperBound 10
+#endif
 
 //macro to get a random float between two values
 #define RandomBetween(min,max) (min + (r32)(rand() / ((r32)(RAND_MAX/(max - min)))))
 #define IsBetween(test, min, max) ((test) < (min) ? 0 : ((test) > (max) ? 0 : 1))
-#define Swap(x,y) do \
-    { u8 swap_temp[sizeof(x) == sizeof(y) ? (signed)sizeof(x) : -1];    \
-       memcpy(swap_temp,&y,sizeof(x)); \
-       memcpy(&y,&x,       sizeof(x)); \
-       memcpy(&x,swap_temp,sizeof(x)); \
-    } while(0)
 
-internal u32
-LotteryWinner(u32 LotteryEntrantsCount)
-{
-    u32 LotteryTicketCount = LotteryEntrantsCount * (LotteryEntrantsCount + 1) / 2;
-
-    u32 ChosenTicket = (u32)(RandomBetween(0, (r32)LotteryTicketCount) + 0.5f);
-    ChosenTicket = (ChosenTicket >= LotteryTicketCount) ? LotteryTicketCount-1 : ChosenTicket;
-
-    u32 WinnerIndex = 0;
-    u32 TicketIndex = ChosenTicket;
-
-    u32 TicketCutoff = PopulationSize;
-    while(TicketIndex >= TicketCutoff)
-    {
-        ++WinnerIndex;
-        TicketIndex -= TicketCutoff--;
-    }
-
-    Assert(WinnerIndex >= 0 && WinnerIndex < PopulationSize);
-    
-    return WinnerIndex;
-}
-
-internal void
-PrintPopulation(population *Population)
-{
-    for(u32 IndividualIndex = 0;
-        IndividualIndex < PopulationSize;
-        ++IndividualIndex)
-    {
-        printf("(%f,%f)\n", Population->Individuals[IndividualIndex].Chromosome[0],
-               Population->Individuals[IndividualIndex].Chromosome[1]);
-    }
-}
-
-internal void
-PrintArray(individual_fitness *Array, u32 Size)
-{
-    LINE_BREAK
-    for(u32 ArrayIndex = 0;
-        ArrayIndex < Size;
-        ++ArrayIndex)
-    {
-        printf("%f\n", Array[ArrayIndex].FitnessScore);
-    }
-    LINE_BREAK
-}
-
-internal void
-Quicksort(individual_fitness *Array, s32 First, s32 Last)
-{
-    s32 Pivot, I, J;
-    individual_fitness Temp;
-
-    if(First < Last)
-    {
-        Pivot = First;
-        I = First;
-        J = Last;
-
-        while(I < J)
-        {
-            while(Array[I].FitnessScore >= Array[Pivot].FitnessScore && I < Last)
-            {
-                ++I;
-            }
-            while(J > Pivot && Array[J].FitnessScore <= Array[Pivot].FitnessScore)
-            {
-                --J;
-            }
-            if(I < J)
-            {
-                Temp = Array[I];
-                Array[I] = Array[J];
-                Array[J] = Temp;
-            }
-        }
-
-        Temp = Array[Pivot];
-        Array[Pivot] = Array[J];
-        Array[J] = Temp;
-        Quicksort(Array, First, J-1);
-        Quicksort(Array, J+1, Last);
-    }
-}
-
-//theoretical max: 1.80738
 internal r32
-FitnessScore(individual *Individual)
+ValueScore(r32 X)
 {
     r32 Result = 0;
 
-    r32 X = Individual->Chromosome[0];
-    r32 Y = Individual->Chromosome[1];
-
-    if (IsBetween(X, LowerBound, UpperBound) && IsBetween(Y, LowerBound, UpperBound))
+    if (IsBetween(X, LowerBound, UpperBound))
     {
-        Result = (r32)(atan(X) + atan(Y) + sin(X) + sin(Y));
+        Result = (r32)(atan(X) + sin(X));
     }
-    
+
     return Result;
 }
 
+//theoretical max from -2 to 4: 1.80738
+//theoretical max from -10 to 10: 7.86987
+internal r32
+FitnessScore(r32 X, r32 Y)
+{    
+    return ValueScore(X) + ValueScore(Y);
+}
+
+#include "ga.h"
+
+#ifndef GA
 internal void
-Mutate(individual *Individual)
+HillClimb()
 {
-    for(u32 ChromosomeIndex = 0;
-        ChromosomeIndex < ChromosomeSize;
-        ++ChromosomeIndex)
+    r32 candidate[5];
+    r32 currentPoint = 0;
+    r32 stepSize = 0.1;
+    r32 acceleration = 1.2;
+    candidate[0] = -acceleration;
+    candidate[1] = -1 / acceleration;
+    candidate[2] = 0;
+    candidate[3] = 1 / acceleration;
+    candidate[4] = acceleration;
+    for(;;)
     {
-        r32 Random = RandomBetween(0,1);
-        if(Random < MutationRate)
-        {
-            r32 Offset = RandomBetween(-1,1);
-            Offset = (r32)pow(Offset, 5);
-            Offset *= MutationMagnitude;
-            Individual->Chromosome[ChromosomeIndex] += Offset;
-        }
-    }
+        r32 before = ValueScore(currentPoint);
+        for each element i in currentPoint do
+                     best = -1;
+        bestScore = -INF;
+        for j from 0 to 4         // try each of 5 candidate locations
+                  currentPoint[i] = currentPoint[i] + stepSize[i] * candidate[j];
+        temp = EVAL(currentPoint);
+        currentPoint[i] = currentPoint[i] - stepSize[i] * candidate[j];
+        if(temp > bestScore)
+            bestScore = temp;
+        best = j;
+        if candidate[best] is not 0
+                        currentPoint[i] = currentPoint[i] + stepSize[i] * candidate[best];
+        stepSize[i] = stepSize[i] * candidate[best]; // accelerate
+        if (EVAL(currentPoint) - before) < epsilon 
+                                           return currentPoint;
+    }    
 }
-
-internal population *
-ReproducePopulation(population *Population)
-{
-    individual_fitness PopulationFitnessRanked[PopulationSize] = {};
-    
-    for(u32 IndividualIndex = 0;
-        IndividualIndex < PopulationSize;
-        ++IndividualIndex)
-    {
-        PopulationFitnessRanked[IndividualIndex].IndividualIndex = IndividualIndex;
-         PopulationFitnessRanked[IndividualIndex].FitnessScore =
-            FitnessScore(&Population->Individuals[IndividualIndex]);
-    }
-
-    Quicksort(PopulationFitnessRanked, 0, PopulationSize-1);
-
-    individual Best = Population->Individuals[PopulationFitnessRanked[0].IndividualIndex];
-    printf("Best Individual: (%f, %f)\n", Best.Chromosome[0], Best.Chromosome[1]);
-
-    population *NewPopulation = (population *)malloc(sizeof(population));
-
-    for(u32 IndividualIndex = 0;
-        IndividualIndex < PopulationSize;
-        ++IndividualIndex)
-    {
-        u32 LotteryWinnerFitnessIndex = LotteryWinner(PopulationSize);
-        u32 LotteryWinnerIndex = PopulationFitnessRanked[LotteryWinnerFitnessIndex].IndividualIndex;
-        individual LotteryWinner = Population->Individuals[LotteryWinnerIndex];
-        Mutate(&LotteryWinner);
-        NewPopulation->Individuals[IndividualIndex] = LotteryWinner;
-    }
-
-    free(Population);
-    
-    return NewPopulation;
-}
-
-internal void
-RandomizePopulation(population *Population)
-{
-    for(u32 IndividualIndex = 0;
-        IndividualIndex < PopulationSize;
-        ++IndividualIndex)
-    {
-        individual *Individual = &Population->Individuals[IndividualIndex];
-        for(u32 ChromosomeIndex = 0;
-            ChromosomeIndex < ChromosomeSize;
-            ++ChromosomeIndex)
-        {
-            Individual->Chromosome[ChromosomeIndex] = RandomBetween(-4,4);
-        }
-    }
-}
+#endif
 
 s32 main()
 {
@@ -208,18 +100,22 @@ s32 main()
     srand(0);
 #endif
 
+#if GA
     population *Population = (population *)malloc(sizeof(population));
-
+    
     RandomizePopulation(Population);
 
     for(u32 IndividualIndex = 0;
         IndividualIndex < GenerationCount;
         ++IndividualIndex)
     {
-        LINE_BREAK
-//        PrintPopulation(Population);
         Population = ReproducePopulation(Population);
     }
+#else
+    HillClimb();
+#endif
+
+    free(Population);
 
     return 1;
 }
